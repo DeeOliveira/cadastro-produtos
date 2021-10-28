59b2d3f4a063ee7530869cb11efa492074419079
Deise Daiane de Oliveira <OliveiraDeiseDaianeD@JohnDeere.com>
2021-10-28 10:25:04 -0300

    Commit

---
+++ src/rotas.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { Switch, Route } from 'react-router-dom'
+
+import Home from './views/home'
+import CadastroProduto from './views/produtos/cadastros'
+import { ConsultaProduto } from './views/produtos/consulta'
+
+export default () => {
+    return (
+        <Switch>
+            <Route exact path="/cadastro-produtos:sku?" component={CadastroProduto} />
+            <Route exact path="/consulta-produtos" component={ConsultaProduto} />
+            <Route exact path="/" component={Home} />
+        </Switch>      
+
+    )
+
+}
+
---
+++ src/views/produtos/consulta.js
@@ -0,0 +1,66 @@
+import React from "react";
+import ProdutoService from "../../app/produtoService";
+import { withRouter } from "react-router"
+
+export class ConsultaProduto extends React.Component {
+
+    state = {
+        produtos : []
+    }
+
+    constructor(){
+        super()
+        this.service = new ProdutoService();
+    }
+
+    componentDidMount(){
+        const produtos = this.service.obterProdutos();
+        this.setState({produtos})
+    }
+
+    preparaEditar = (sku) =>{
+        this.props.history.push(`/cadastro-produtos/${sku}`)
+    }
+    
+    render(){
+        return(
+            <div className="card">
+                <div className="card-header">
+                    Consulta de Produto
+                </div>
+                <div className="card-body">
+                    <table className="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>Nome</th>
+                                <th>SKU</th>
+                                <th>Preço</th>
+                                <th>Fornecedor</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.produtos.map((produto, index) =>{
+                                return(
+                                    <tr key={index}>
+                                    <th>{produto.nome}</th>
+                                    <th>{produto.sku}</th>
+                                    <th>{produto.preco}</th>
+                                    <th>{produto.fornecedor}</th>
+                                    <th>
+                                        <button onClick={() => this.preparaEditar(produto.sku)} className="btn btn-primary">Editar</button>
+                                        <button className="btn btn-danger">Remover</button>
+                                    </th>
+                                </tr>
+                                )
+                            })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        )
+    }
+}
+
